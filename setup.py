7234b8546d479e8802ad090933ab892d4861f7d3
Jannis Leidel <jannis@leidel.info>
2017-05-02 15:25:19 +0200

    Remove Appveyor.

--- setup.py
+++ setup.py
@@ -146,7 +146,7 @@ setuptools.setup(
     platforms=['any'],
     license='BSD',
     classifiers=classifiers,
-    install_requires=reqs('default.txt'),
+    install_requires=reqs('default.txt') + reqs('django.txt'),
     tests_require=reqs('test.txt'),
     cmdclass={'test': pytest},
     zip_safe=False,
